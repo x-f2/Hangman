73d6219ecc78a3baeb69d18f421422b84bee6a06
x-f2 <113227770+x-f2@users.noreply.github.com>
2022-10-15 14:45:49 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 ##
 # Tests Hangman.py Application
-# If not arguments given, defaults to a random sentence
+# If no arguments are given, defaults to a random sentence
 #   in the file ./random-sentences.txt
 # Else, uses the evaluated result of proceeding string as sh command
 #   e.g. "./test.sh echo This is my phrase" or "./test.sh fortune"
